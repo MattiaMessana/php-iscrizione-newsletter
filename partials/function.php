73352ef5bb0f2bcb77ea8ea48ff84c4e625d36d8
MattiaMessana <mattia.messana@icloud.com>
2024-05-23 12:57:10 +0200

    add: control email empty

--- partials/function.php
+++ partials/function.php
@@ -1,10 +1,15 @@
 <?php
 if (isset($_GET["control_email"])) {
-    $is_at = strpos($_GET['control_email'], '@') !== false;
-    $is_dot = strpos($_GET['control_email'], '.') !== false;
-    if ($is_at && $is_dot) {
-        echo '<div class="alert alert-success w-50" role="alert">Email Valida</div>';
+    //controllo se l'utente abbiamo inserito qualcosa nel input dopo che ha cliccato il pulsante submit
+    if (empty($_GET['control_email'])) {
+        echo '<div class="alert alert-danger w-50" role="alert">Inserisci Email</div>';
     } else {
-        echo '<div class="alert alert-danger w-50" role="alert">Email Non Valida</div>';
+        $is_at = strpos($_GET['control_email'], '@') !== false;
+        $is_dot = strpos($_GET['control_email'], '.') !== false;
+        if ($is_at && $is_dot) {
+            echo '<div class="alert alert-success w-50" role="alert">Email Valida</div>';
+        } else {
+            echo '<div class="alert alert-danger w-50" role="alert">Email Non Valida</div>';
+        }
     }
 }
